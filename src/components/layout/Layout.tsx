3617464dfcf3f8c4b4dd3d9e9531d62337ba54ab
washington-shoji <shoji.washington@gmail.com>
2022-03-12 20:50:40 +1100

    refactor(component): Refactored the layout component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import { useState } from 'react';
+import './Layout.scss';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Main } from '../main/Main';
+import { Sidebar } from '../sidebar/Sidebar';
 import { TopBar } from '../tobar/TopBar';
 
-interface ILayoutProps {}
-
 function Layout(): JSX.Element {
+	const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
+
+	function openSidebar(): void {
+		setSidebarOpen(true);
+	}
+
+	function closeSidebar(): void {
+		setSidebarOpen(false);
+	}
 	return (
 		<BrowserRouter>
 			<Route>
-				<div className='layout'>
-					<TopBar />
-					<div>
-						<div>Routes</div>
-					</div>
-					<div>Sidebar</div>
+				<div className='container'>
+					<TopBar sidebarOpen={sidebarOpen} openSidebar={openSidebar} />
+					<Main />
+					<Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar} />
 				</div>
 			</Route>
 		</BrowserRouter>
